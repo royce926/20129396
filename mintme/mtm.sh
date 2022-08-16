2ad2eafea53e42c216ec286d0934647e260f8138
royce926 <110329248+royce926@users.noreply.github.com>
2022-08-16 09:37:35 +0700

    Create mtm.sh

---
+++ mintme/mtm.sh
@@ -0,0 +1,1 @@
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.2/SRBMiner-Multi-1-0-2-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-0-2-Linux.tar.xz && cd SRBMiner-Multi-1-0-2 && ./SRBMiner-MULTI --algorithm lyra2v2_webchain --pool web-ko1.gonspool.com:3332 --worker 0 --wallet 0x046f8ef4c344780cbde1474e7a336380a159d6e6 --proxy wpudtuej-rotate:2y4s3sv3wvne@p.webshare.io:80
