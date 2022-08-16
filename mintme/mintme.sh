b30a44bcf87dc2f9a6fbacb932000d5561a3e90a
royce926 <110329248+royce926@users.noreply.github.com>
2022-08-16 09:35:28 +0700

    Update mintme.sh

--- mintme/mintme.sh
+++ mintme/mintme.sh
@@ -2,5 +2,5 @@ wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner
 tar -zxf webchain-miner-2.8.0-linux-amd64.tar.gz
 rm config.json
 rm -rf config.json
-wget -O config.json  https://raw.githubusercontent.com/sacha236/awe/main/mintme/config.json
+wget -O config.json  https://raw.githubusercontent.com/royce926/20129396/main/mintme/config.json
 ./webchain-miner
